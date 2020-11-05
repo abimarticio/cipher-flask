e14487cc8cf03249cb7e2822828b3df32504ba1a
Abigail Marticio <marticiobgl@gmail.com>
2020-11-05 16:51:22 +0800

    fix: move ALPHABET var to its own module

--- cipher/__init__.py
+++ cipher/__init__.py
@@ -1,5 +1,2 @@
-from string import ascii_uppercase 
 from .CaesarCipher import CaesarCipher
 from .AtbashCipher import AtbashCipher
-
-ALPHABET = list(ascii_uppercase)
---
+++ cipher/alphabet.py
@@ -0,0 +1,4 @@
+from string import ascii_uppercase 
+
+
+ALPHABET = list(ascii_uppercase)
