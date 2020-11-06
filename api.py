ba7b62961464078e07960209da81487f3be6bf41
Abigail Marticio <marticiobgl@gmail.com>
2020-11-06 23:13:45 +0800

    chore: add view function for atbash encryption

--- api.py
+++ api.py
@@ -1,8 +1,14 @@
 from flask import Flask, request
+from cipher import AtbashCipher, CaesarCipher
 
 
 app = Flask(__name__)
 
+@app.route("/atbash/encrypt", methods=["GET"])
+def atbash_encrypt():
+    text = request.args.get("text", default=None)
+    encrypted_text = AtbashCipher()
+    return f"text: {text} - encrypted text: {encrypted_text(text)}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
