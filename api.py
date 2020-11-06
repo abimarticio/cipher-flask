dcf67089282f72835dbc27a1b381ef8598e610dd
Abigail Marticio <marticiobgl@gmail.com>
2020-11-06 23:15:16 +0800

    chore: add view function for Caesar encryption

--- api.py
+++ api.py
@@ -16,5 +16,13 @@ def atbash_decrypt():
     decrypted_text = AtbashCipher()
     return f"text: {text} - decrypted text: {decrypted_text(text)}"
 
+@app.route("/caesar/encrypt", methods=["GET"])
+def caesar_encrypt():
+    text = request.args.get("text", default=None)
+    mode = request.args.get("mode", default="encrypt")
+    key = request.args.get("key", default=None, type=int)
+    encrypted_text = CaesarCipher()
+    return f"text: {text} - encrypted text: {encrypted_text(text, mode, key)}"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
