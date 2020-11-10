a491dc132e39a5ad1df69eee7e6c65bfe7e3e1ff
Abigail Marticio <marticiobgl@gmail.com>
2020-11-10 17:53:13 +0800

    chore: add method for decryption

--- cipher/CaesarCipher.py
+++ cipher/CaesarCipher.py
@@ -31,3 +31,14 @@ class CaesarCipher:
             output_letter = self.alphabet[output_index]
             encrypted_text.append(output_letter)
         return "".join(encrypted_text)
+
+    def decrypt_text(self, text: str, key: int) -> str:
+        alphabet_len = len(self.alphabet)
+        text = text.upper()
+        decrypted_text = []
+        for letter in text:
+            letter_index = self.alphabet.index(letter)
+            output_index = (letter_index - key) % alphabet_len
+            output_letter = self.alphabet[output_index]
+            decrypted_text.append(output_letter)
+        return "".join(decrypted_text)
