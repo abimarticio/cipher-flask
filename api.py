e3e4a77b106d2723317b1f9a8ffde317a98db225
Abigail Marticio <marticiobgl@gmail.com>
2020-11-10 18:11:40 +0800

    chore: add changes for calling method

--- api.py
+++ api.py
@@ -24,8 +24,8 @@ def caesar_encrypt():
     text = request.args.get("text", default=None)
     key = request.args.get("key", default=None, type=int)
     Caesar = CaesarCipher()
-    encrypted_text = CaesarCipher()
-    return f"text: {text} - encrypted text: {encrypted_text(text, key)}"
+    encrypted_text = Caesar.encrypt_text(text, key)
+    return f"text: {text} - encrypted text: {encrypted_text}"
 
 
 @app.route("/caesar/decrypt", methods=["GET"])
@@ -33,8 +33,8 @@ def caesar_decrypt():
     text = request.args.get("text", default=None)
     key = request.args.get("key", default=None, type=int)
     Caesar = CaesarCipher()
-    decrypted_text = CaesarCipher()
-    return f"text: {text} - decrypted text: {decrypted_text(text, key)}"
+    decrypted_text = Caesar.decrypt_text(text, key)
+    return f"text: {text} - decrypted text: {decrypted_text}"
 
 
 if __name__ == "__main__":
